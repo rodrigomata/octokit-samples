e697cb567c0f53a91356c5d51deace91311c3d88
royestone <rodrigo.mata.ib@gmail.com>
2022-01-06 14:59:05 -0600

    feat: added rest and api options to test release workflows

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
+require('dotenv').config();
 const { Octokit } = require("octokit");
 
-(function() {
+const {
+  GITHUB_TOKEN = '',
+  GITHUB_REPO = ''
+} = process.env
+
+const octokit = new Octokit({ auth: GITHUB_TOKEN });
+
+(async function() {
   console.log('start');
-  const octokit = new Octokit({ auth: `personal-access-token123` });
+
+  // :: Login
+  const owner = await getAuthenticated()
+  
+  // :: Workflows
+  const workflowRuns = await getWorkflows({ owner, repo: GITHUB_REPO, workflow_id: 'release.yml' })
+  // const all = await getAllWorkflows({ owner, repo: GITHUB_REPO })
+  // console.log('workflowRuns', workflowRuns)
+  
+  console.log('end');
 })();
+
+async function getAuthenticated() {
+  const {
+    data: { login },
+  } = await octokit.rest.users.getAuthenticated();
+  return login;
+}
+
+async function getWorkflows({ owner, repo, workflow_id, branch = 'main' }) {
+  const { data: { workflow_runs, total_count } } = await octokit
+    .request(`GET /repos/${owner}/${repo}/actions/workflows/${workflow_id}/runs`, {
+      owner,
+      repo,
+      // branch,
+      // workflow_id,
+      event: 'release'
+    })
+  console.log({ total_count })
+  return workflow_runs
+}
+
+async function getAllWorkflows({ owner, repo }) {
+  const { data: { workflow_runs } } = await octokit.rest.actions.listWorkflowRunsForRepo({
+    owner,
+    repo,
+    event: 'release'
+  });
+  console.log('runs', JSON.stringify(workflow_runs[0], null, 2));
+}
